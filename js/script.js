f82b60a775f60d1eeb2197a934a71605da6701df
Vlad Tarasenko <tarasenkovlady@gmail.com>
2017-06-09 17:57:11 +0300

    Add files via upload
    1

---
+++ js/script.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function() {
+$(".link_block1").mouseout(function(){
+$(this).text("Мышка УШЛА :( ").css("display", "none")
+});
+$(".link_block1").mouseover(function(){
+$(this).text("Мышка ПРИШЛА!!! :)").css("display", "block")
+});
+                              });
